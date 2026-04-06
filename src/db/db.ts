bc765f97ab6aca585fbf2677e248b5095684eb5f
Ben <34235864+benjaminfkile@users.noreply.github.com>
2026-04-05 20:08:12 -0500

    Revert "[WIP] Set up knex migrations infrastructure (#57)"
    This reverts commit 2180b09a9a402683cfef90410a31933366e0bedb.

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,5 +1,4 @@
 import knex, { Knex } from "knex";
-import path from "path";
 import { IAppSecrets, IDBSecrets } from "../interfaces";
 import health from "./health";
 
@@ -26,17 +25,10 @@ export async function initDb(
       port: 5432,
       ssl: { rejectUnauthorized: false },
     },
-    migrations: {
-      directory: path.join(__dirname, "migrations"),
-    },
   });
 
   const dbHealth = await health.getDBConnectionHealth(db, true);
   console.log(dbHealth.logs);
-
-  if (appSecrets.NODE_ENV !== "production") {
-    await db.migrate.latest();
-  }
 
   return db;
 }
